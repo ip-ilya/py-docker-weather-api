b5df49d5c9ff2fb362dcda850eb25cd5e78f4488
Illia Khomutov <ilyakhomutov613@gmail.com>
2025-04-27 22:39:14 +0300

    Solution

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM python:3.11.6-alpine3.18
+LABEL maintainer="ilyakhomutov613@gmail.com"
+
+ENV PYTHONBUFFERED=1
+
+WORKDIR /app
+
+COPY requirements.txt .
+RUN pip install -r requirements.txt
+
+COPY . .
+
+CMD ["python", "-m", "app.main"]
--- app/main.py
+++ app/main.py
@@ -1,6 +1,17 @@
+from dotenv import load_dotenv
+import requests
+import os
+
+load_dotenv()
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = "https://api.weatherapi.com/v1/current.json"
+    api_key = os.getenv("API_KEY")
+    city = "Paris"
+    response_json = requests.get(f"{url}?key={api_key}&q={city}").json()
+    print(f"Celsius: {response_json['current']['temp_c']}")
+    print(f"Fahrenheit: {response_json['current']['temp_f']}")
 
 
 if __name__ == "__main__":
